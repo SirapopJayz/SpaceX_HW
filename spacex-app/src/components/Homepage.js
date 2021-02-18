f696477d4c3ab4ea794bd614bcbff81246ef2e40
sirapopjayz <Jayza130443@gmail.com>
2021-02-19 04:03:41 +0700

    update css video

--- spacex-app/src/components/Homepage.js
+++ spacex-app/src/components/Homepage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../index.css"
 import "./Homepage.css";
-import video1 from "../components/videos/video-1.mp4"
+import Video1 from "../components/videos/video-1.mp4"
 const Homepage = () => {
   return (
       <div className='home-container'>
         <video loop autoPlay muted preload="auto" autobuffer="true" data-mobile-video>
-          <source src={video1} type="video/mp4" />
+          <source src={Video1} type="video/mp4" />
         </video>
-        <h1 style={{color: 'white'}}>SpaceX</h1>
+        <h1>SpaceX</h1>
       </div>
   );
 };
