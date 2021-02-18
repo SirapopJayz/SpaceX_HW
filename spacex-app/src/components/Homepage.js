eeaef460ccff36aa3c7d0a1711c733c2b2cb07a5
Champchips <otakuchamp@gmail.com>
2021-02-19 00:24:27 +0700

    fetch api https://api.spacexdata.com/v3/info

--- spacex-app/src/components/Homepage.js
+++ spacex-app/src/components/Homepage.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Homepage.css";
+import { useQuery } from "react-query";
 const Homepage = () => {
+  const { isLoading, error, data } = useQuery('spaceX', () =>
+     fetch('https://api.spacexdata.com/v3/info').then(res =>
+       res.json()
+     )
+   )
+   if (isLoading) return 'Loading...'
+   if (error) return 'An error has occurred: ' + error.message
+   console.log(data);
   return (
-      <h1>This is HomePage</h1>
+      <div className="content">
+        <h3>Founder : {data.founder}</h3>
+        <h3>Elon's Twitter : <a href={data.links.elon_twitter}><i className="fab fa-twitter"></i></a></h3>
+        <h4>Address : {data.headquarters.address}</h4>
+        <h4>City : {data.headquarters.city}</h4>
+        <h4>State : {data.headquarters.state}</h4><br></br>
+        <p>Summary: {data.summary}</p><br></br>
+        <p>Official WebPage: <a href={data.links.website}>SpaceX<i className="fas fa-rocket"></i></a></p>
+      </div>
   );
 };
 export default Homepage;
--- spacex-app/src/index.js
+++ spacex-app/src/index.js
@@ -6,16 +6,20 @@ import Launchespage from "./components/Launchespage";
 import Rocketpage from "./components/Rocketpage";
 import Navbar from "./components/Navbar/Navbar";
 import "./index.css";
+import { QueryClient, QueryClientProvider} from "react-query";
 import { BrowserRouter, Redirect, Route } from "react-router-dom";
+const queryClient = new QueryClient();
 ReactDOM.render(
-  <div className="body">
-    <BrowserRouter>
-      <Navbar />
-      <Route exact path="/Home" component={Homepage} />
-      <Route path="/Launches" component={Launchespage} />
-      <Route path="/Rocket" component={Rocketpage} />
-      <Redirect to="/Home" />
-    </BrowserRouter>
-  </div>,
+  <QueryClientProvider client={queryClient}>
+    <div className="body">
+      <BrowserRouter>
+        <Navbar />
+        <Route exact path="/Home" component={Homepage} />
+        <Route path="/Launches" component={Launchespage} />
+        <Route path="/Rocket" component={Rocketpage} />
+        <Redirect to="/Home" />
+      </BrowserRouter>
+    </div>
+  </QueryClientProvider>,
   document.getElementById("root")
 );
