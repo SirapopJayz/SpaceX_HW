4f758f78487d0ee2f936ddfb4ee0e85dee261f6a
sirapopjayz <Jayza130443@gmail.com>
2021-02-21 19:04:28 +0700

    Update css detailrocket

--- spacex-app/src/components/Detailpage/Rocket.js
+++ spacex-app/src/components/Detailpage/Rocket.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useQuery } from "react-query";
+import "./Rocket.css";
+import Video2 from "../../components/videos/video-2.mp4"
 
 const Rocket = (props) => {
   const { isLoading, error, data } = useQuery("spaceX", () =>
@@ -11,43 +13,52 @@ const Rocket = (props) => {
   if (error) return "An error has occurred: " + error.message;
   console.log(data);
   return (
-    <div className="content">
-      <img
-        src={data.flickr_images[data.flickr_images.length - 1]}
-        alt="Rocket"
-      />
-      <p>RocketName : {data.rocket_name}</p>
-      <p>Country : {data.country}</p>
-      <p>Description :{data.description}</p>
-      <p>Booster: {data.boosters}</p>
-      <p>Cost: {data.cost_per_launch}</p>
-      Diameter:
-      <ul>
-        <li>Feet: {data.diameter.feet}</li>
-        <li>meters: {data.diameter.meters}</li>
-      </ul>
-      Mass:
-      <ul>
-        <li>Kg: {data.mass.kg}</li>
-        <li>Lb: {data.mass.lb}</li>
-      </ul>
-      Height:
-      <ul>
-        <li>Meters: {data.height.meters}</li>
-        <li>Feet: {data.height.feet}</li>
-      </ul>
-      <ul>
-        Payload weights:
-        {data.payload_weights.map((item) => {
-          return (
+    <div className="container">
+      <video loop autoPlay muted preload="auto" autobuffer="true" data-mobile-video>
+                <source src={Video2} type="video/mp4" />
+              </video>
+      <div className="bg">
+        <div className="img-conainer">
+          <img
+            src={data.flickr_images[data.flickr_images.length - 1]}
+            alt="Rocket"
+          />
+        </div>
+        <div className="context-container">
+          <div className="context">
+            <h2>{data.rocket_name}</h2>
+            <h5><strong>Country :</strong> {data.country}</h5>
+            <h5><strong>Description :</strong>{data.description}</h5>
+            <h5><strong>Booster :</strong> {data.boosters}</h5  >
+            <h5><strong>Cost :</strong> {data.cost_per_launch}</h5>
+            Diameter:
             <ul>
-              <li>Name: {item.name}</li>
-              <li>Kg: {item.kg}</li>
-              <li>Lb: {item.lb}</li>
+              <li style={{marginLeft:20}}>Feet: {data.diameter.feet}</li>
+              <li style={{marginLeft:20}}>meters: {data.diameter.meters}</li>
             </ul>
-          );
-        })}
-      </ul>
+            Mass:
+            <ul>
+              <li style={{marginLeft:20}}>Kg: {data.mass.kg}</li>
+              <li style={{marginLeft:20}}>Lb: {data.mass.lb}</li>
+            </ul>
+            Height:
+            <ul>
+              <li style={{marginLeft:20}}>Meters: {data.height.meters}</li>
+              <li style={{marginLeft:20}}>Feet: {data.height.feet}</li>
+            </ul>
+            Payload weights:
+            {data.payload_weights.map((item) => {
+              return (
+                <ul>
+                  <li style={{marginLeft:20}}>Name: {item.name}</li>
+                  <li style={{marginLeft:20}}>Kg: {item.kg}</li>
+                  <li style={{marginLeft:20}}>Lb: {item.lb}</li>
+                </ul>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
